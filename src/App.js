66b5d80d21e06bd8e09526232593ee7e379bce0e
Masrafi Apurba <101468860@georgebrown.ca>
2026-02-13 23:59:20 -0500

    fix typo in App.js

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Studen ID: 101468860</h1>
-      <h1>Studen ID: Masrafi Apurba</h1>
+      <h1>Student ID: 101468860</h1>
+      <h1>Student Name: Masrafi Apurba</h1>
       <h1>Student Information System</h1>
       <h5>Powered By: Alpha Man Labs Inc. || DevOps</h5>
       <StudentForm addStudent={addStudent} />
-      Developerd By: Mashrafi Apurba || Student ID: 101468860
+      Developed By: Masrafi Apurba || Student ID: 101468860
       <StudentList students={students} />
     </div>
   );
